420ff1d265af8abe55b220bbb80408d99029fb43
Nguyen Danh <khanhnguyen1@tdt.asia>
2022-03-06 17:06:52 +0700

    fix logic auto ON OFF

--- src/mqtt/clientMqtt.ts
+++ src/mqtt/clientMqtt.ts
@@ -32,7 +32,7 @@ client.on('connect', async () => {
         });
         client.subscribe(['APPr/notification/' + obj.mac_address], () => {
             console.log(`Subscribe to topic APPr/notification/` + obj.mac_address)
-        }); 
+        });
         client.subscribe(['ESPs/status/' + obj.mac_address], () => {
             console.log(`Subscribe to topic ESPs/status/` + obj.mac_address)
         });
@@ -53,16 +53,43 @@ client.on('message', (topic, payload) => {
     if (iTopic == 'ESPs/status') {
         status(payload.toString(), topicArr[2]);
     }
+    if (iTopic == 'ESPs/RL') {
+        warning(payload.toString(), topicArr[2])
+    }
 })
+
+async function warning(payload: string, mac: string) {
+    const objPlayload = JSON.parse(payload);
+
+    const snapshot = await firestore.collection("EspRelay").where('mac_address', '==', mac).get()
+
+    snapshot.forEach((snap) => {
+        const obj = JSON.parse(JSON.stringify(snap.data()));
+        console.log(obj);
+        if (obj.relay_id == objPlayload.relay_id && obj.humidity_warning > objPlayload.soil_humidity) {
+            const msg = {
+                Timestamp: moment().format('YYYY-MM-DDTHH:mm:ss'),
+                Message: "Relay " + obj.relay_name + " đang có độ ẩm là " + objPlayload.humidity + ". Vui lòng bơm nước!"
+            }
+            client.publish("APPr/notification/" + mac, JSON.stringify(msg));
+
+            if (obj.auto_on_off == true) {
+                client.publish(`ESPn/RL1${mac}`, "1");
+                setTimeout(() => client.publish(`ESPn/RL1${mac}`, "0"), Number(objPlayload.water_time) * 1000)
+            }
+
+        }
+    })
+}
 
 async function status(payload: string, mac: string) {
     const objPlayload = JSON.parse(payload);
 
     const res = await firestore.collection("EspRelayStatus").add({
         relay_id: objPlayload.relay_id,
-        water_time: objPlayload.water_time/1000,
+        water_time: objPlayload.water_time / 1000,
         water_amount: objPlayload.water_amount,
-        mac_address: mac,  
+        mac_address: mac,
         timestamp: moment().format('YYYY-MM-DDTHH:mm:ss')
     });
 
@@ -74,7 +101,6 @@ async function environment(payload: string, mac: string) {
 
     const res = await firestore.collection("EspEnvironment").add({
         humidity: objPlayload.humidity,
-        relay_id: objPlayload.relay_id,
         mac_address: mac,
         soil_humidity: objPlayload.soil_humidity,
         temperature: objPlayload.temperature
